afddd230adb4ff513aa62943f8f6940104e056e8
Sahib Bajaj <sbajaj7@users.noreply.github.com>
2025-11-29 20:51:45 -0800

    Update Unique_Paths_62.js

--- Unique_Paths_62.js
+++ Unique_Paths_62.js
@@ -16,3 +16,37 @@
     }
 };*/
 
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    let memo = []
+    for(let i = 0;i<m; i++) {
+        memo[i] = []
+    }
+
+    return helper(0,0, memo)
+
+    function helper(row, col, memo) {
+        if(row == m-1 && col == n-1) {
+            return 1
+        }
+        //This is required above the next if condition bcz there will be out of bounds exception
+        if(row == m || col == n) {
+            return 0
+        }
+        if(memo[row][col] != undefined) {
+            return memo[row][col]
+        }
+        let right = helper(row,col+1, memo)
+        let down = helper(row+1,col, memo)
+
+        memo[row][col] = right + down
+        return memo[row][col]
+    }
+};
+
+
+
