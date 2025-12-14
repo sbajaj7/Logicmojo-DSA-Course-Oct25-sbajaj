eef6547697193046910a2f18cea597b0c049a973
Sahib Bajaj <sbajaj7@users.noreply.github.com>
2025-12-13 21:13:20 -0800

    Create Same Tree_100.java
    Dec 13 2025

---
+++ Same Tree_100.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)
+            return true;
+        else if(p==null)
+            return false;
+        else if(q==null)
+            return false;
+
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
